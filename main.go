326408561debe75ee7472b8d781789013ca635dd
gcoder <gcoder@live.com>
2020-12-18 08:16:34 +0800

    modify json

--- main.go
+++ main.go
@@ -25,15 +25,16 @@ func main() {
 	destPath := "./res/out/"
 
 	srcFile := "test.flv"
-	// destFile := "test.mp4"
-	// cmdTranscode := []byte(`{"raw":["-vf scale=-2:960","-c:v libx264","-profile:v main","-level:v 3.1","-x264opts scenecut=0:open_gop=0:min-keyint=72:keyint=72","-minrate 1000k","-maxrate 1000k","-bufsize 1000k","-b:v 1000k","-y"]}`)
-	// err := ffmpeg.Transcode(srcPath, srcFile, destPath, destFile, string(cmdTranscode[:]))
-	// cmdRemux := []byte(`{"raw":["-c copy","-y"]}`)
-	// err := ffmpeg.Transcode(srcPath, srcFile, destPath, destFile, string(cmdRemux[:]))
-	cmdSplit := []byte(`{"raw":["-c copy", "-f segment", "-segment_time 5", "-reset_timestamps 1", "-map 0:0", "-map 0:1", "-y"]}`)
-	err := ffmpeg.Transcode(srcPath, srcFile, destPath, "test%d.mp4", string(cmdSplit[:]))
+	destFile := "test.mp4"
+	cmdTranscode := `{"raw":["-vf scale=-2:960","-c:v libx264","-profile:v main","-level:v 3.1","-x264opts scenecut=0:open_gop=0:min-keyint=72:keyint=72","-minrate 1000k","-maxrate 1000k","-bufsize 1000k","-b:v 1000k","-y"]}`
+	err := ffmpeg.Transcode(srcPath, srcFile, destPath, destFile, cmdTranscode)
+	// cmdRemux := `{"raw":["-c copy","-y"]}`
+	// err := ffmpeg.Transcode(srcPath, srcFile, destPath, destFile, cmdRemux)
+	// cmdSplit := `{"raw":["-c copy", "-f segment", "-segment_time 5", "-reset_timestamps 1", "-map 0:0", "-map 0:1", "-y"]}`
+	// err := ffmpeg.Transcode(srcPath, srcFile, destPath, "test%d.mp4", cmdSplit)
 	if err != nil {
 		log.Error("transcode err")
 	}
 	// ffmpeg.DelFile(dest)
+
 }
